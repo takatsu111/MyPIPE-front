74720471a56e05662ed831e722e12340850a1cbc
taka <>
2020-10-02 21:31:20 +0900

    s3にデプロイする実装

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
+  mode: "spa",
+
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
